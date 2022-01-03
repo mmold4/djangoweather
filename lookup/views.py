adc3142fc4379915f9c3484a227a1c66e18919f3
Mike Moldoch <moldochm@hotmail.com>
2022-01-03 18:53:24 -0500

    added api info to app

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=06320&distance=25&API_KEY=F5B9B468-7698-421C-8A35-3226296AA89F")
+
+	try:
+		api = json.loads(api_request.content)
+
+	except Exception as e:
+		api = "Error..."
+
+	return render(request, 'home.html', {'api': api})
+
 
 def about(request):
 	return render(request, 'about.html', {})
